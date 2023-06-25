d7478c928d238bcb5d3bf8db9e5bcfab5e488ca0
Birender07 <120732252+Birender07@users.noreply.github.com>
2023-06-25 18:07:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const myFirstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Hello,world!"); 
+  }, 1000);
+});
+let div = document.querySelector("div");
+myFirstPromise.then((successMessage) => {
+  div.insertAdjacentHTML("afterbegin", `<p>${successMessage}</p>`)
+});
